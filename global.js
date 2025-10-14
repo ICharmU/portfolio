e8f8b8b9020d98084ce261487f2bb7131c38a520
Sean Harmon <99941075+ICharmU@users.noreply.github.com>
2025-10-13 21:32:55 -0700

    dark mode, improved email

--- global.js
+++ global.js
@@ -30,8 +30,53 @@ for (let p of pages) {
         "target",
         a.host !== location.host,
     );
-
 }
+
+preferredColorScheme = matchMedia("(prefers-color-scheme: light)").matches ? "Light" : "Dark";
+document.body.insertAdjacentHTML(
+    "afterbegin",
+    `
+    <label class="color-scheme">
+        Theme:
+        <select>
+            <option value="light dark">Automatic (${preferredColorScheme})</option>
+            <option value="light">Light</option>
+            <option value="dark">Dark</option>
+        </select>
+    </label>
+    `,
+);
+
+const setColorScheme = (preference) => {
+    document.documentElement.style.setProperty("color-scheme", preference);
+}
+
+select = document.querySelector("select");
+select.addEventListener("input", event => {
+    preference = event.target.value
+    setColorScheme(preference);
+    localStorage.colorScheme = preference;
+    // console.log(`color scheme changed to ${event.target.value}`);
+});
+
+if ("colorScheme" in localStorage) {
+    preference = localStorage.colorScheme;
+    setColorScheme(preference);
+    select.value = preference;
+}
+
+form = document.querySelector("form");
+form?.addEventListener("submit", event => {
+    event.preventDefault();
+    data = new FormData(form);
+    build_url = `${form.action}?`;
+    for (let [name, value] of data) {
+        value = encodeURIComponent(value);
+        build_url += `${name}=${value}&`;
+        console.log(build_url);
+    }
+    location.href = build_url;
+});
 
 
 
